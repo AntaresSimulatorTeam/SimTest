e72aa59daa1493635ecf8678a58d37b9079f94de
Florian OMNES <florian.omnes@rte-france.com>
2022-02-22 15:29:50 +0100

    Provide directory for antares-solver

--- scripts/generate_reference.py
+++ scripts/generate_reference.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-
+import re
 import antares_test_utils as antares_utils
 
 parser = argparse.ArgumentParser()
@@ -13,6 +13,16 @@ parser.add_argument("solver", help="Path to antares-solver",
 args = parser.parse_args()
 root = Path(args.root).resolve()
 solver_path = Path(args.solver).resolve()
+
+if (solver_path.is_dir()):
+    results = []
+    for x in solver_path.iterdir():
+        if x.is_file() and re.match("^antares-[0-9]+\.[0-9]+-solver$", x.name):
+            results.append(x)
+    assert(len(results) == 1)
+    solver_path = results[0].resolve()
+
+print(f"Using solver {solver_path}")
 
 if not solver_path:
     raise RuntimeError("Missing solver")
