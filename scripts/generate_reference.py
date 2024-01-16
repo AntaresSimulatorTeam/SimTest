839de1188d5e2aabdcbc6275765cd0544f842622
Florian OMNES <26088210+flomnes@users.noreply.github.com>
2024-01-16 09:47:03 +0100

    Still print OK, KO

--- scripts/generate_reference.py
+++ scripts/generate_reference.py
@@ -56,4 +56,6 @@ for study in studies:
     result = antares_utils.generate_reference_values(solver_path, study, use_ortools, opt_solver, named_mps_problems)
     ret.append(result)
 
+
+    print('OK' if result else 'KO')
 sys.exit(all(ret))
